ae1c6b387932adb364178cfcc77040ec1151a799
Ibrahim Tamtama Adi <ibrahimtamtama17@gmail.com>
2018-08-30 21:22:50 +0700

    commit webnya

---
+++ web/ansible/head.php
@@ -0,0 +1,16 @@
+	<meta charset="utf-8">
+	<title>Mini Server Controller</title>
+	<link rel="icon" href="asset/icon.png">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="js/popper.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" charset="utf8" src="js/jquery.dataTables.js"></script>
+	<style type="text/css">
+		body {
+			font-family: courier, serif !important;
+			font-size: 16px;
+		}
+	</style>
